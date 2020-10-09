e5b7bee9f5d187126c0daa869459feed88c05060
MKalashnikau <mikita@fortvision.com>
2020-10-10 02:17:03 +0300

    refactor commit

--- index.js
+++ index.js
@@ -1,10 +1,15 @@
-const express = require('express')
-const path = require('path')
-const PORT = process.env.PORT || 5000
+const express = require('express');
+const path = require('path');
+const PORT = process.env.PORT || 5000;
+const app = express();
 
-express()
-  .use(express.static(path.join(__dirname, 'public')))
-  .set('views', path.join(__dirname, 'views'))
-  .set('view engine', 'ejs')
-  .get('/', (req, res) => res.render('pages/index'))
-  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
+app.get('/api/getStatus', (req, res) => {
+  res.json({
+    name: "Pasha",
+    status: "Eblan"
+  })
+});
+
+app.listen(PORT, () => {
+  console.log(`Server successfully started on ${PORT}`)
+});
